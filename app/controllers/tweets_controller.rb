ceec8b9a9448883c980cb34a34cae8ddf86d12a1
phyirome <hirohiroxmt@gmail.com>
2020-09-16 17:33:44 +0900

    ツイート投稿処理の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,15 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+  def craete
+    # @tweet = Tweet.new(tweet_params)
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :text, :image)
+  end
+
+
 end
